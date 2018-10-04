a9fdec74b5df586fbfdaa881b9be23d2b508fe14
zhdaa <690539306@qq.com>
2018-10-04 23:49:19 +0800

    添加项目文件。

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Client
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void delegate1(string str);//定义委托
+
+        Socket socketSend;
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //创建负责通信的Socket
+                socketSend = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Parse(txtServer.Text);
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+                //获得要连接的远程服务器应用程序的IP地址和端口号
+                socketSend.Connect(point);
+                ShowMsg("连接成功");
+
+                //开启一个新的线程不停地接收服务器端发来的消息
+                Thread th = new Thread(Receive);
+                th.IsBackground = true;
+                th.Start();
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// 不停地接收服务器发来的消息
+        /// </summary>
+        void Receive()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 3];
+                    //实际接收到的有效字节数
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    string str = Encoding.UTF8.GetString(buffer, 0, r);
+                    Dispatcher.BeginInvoke(DispatcherPriority.Normal, new delegate1(ShowMsg), socketSend.RemoteEndPoint.ToString() + ":" + str);
+                }
+                catch { }
+            }
+        }
+
+        void ShowMsg(string str)
+        {
+            txtLog.AppendText(str + "\r\n");
+        }
+
+        /// <summary>
+        /// 客户端给服务器发送消息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSendMsg_Click(object sender, RoutedEventArgs e)
+        {
+            string str = txtSendMsg.Text.Trim();
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+            socketSend.Send(buffer);
+        }
+    }
+}
---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Server
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void delegate1(string str);//定义委托
+
+        private void btnListen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //当点击开始监听的时候，在服务器端创建一个负责监听IP地址跟端口号的Socket
+                Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Parse("192.168.0.102");//IPAddress.Any;
+                //创建端口号对象
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+                //监听
+                socketWatch.Bind(point);
+                ShowMsg("监听成功");
+                socketWatch.Listen(10);
+
+                Thread th = new Thread(Listen);
+                th.IsBackground = true;
+                th.Start(socketWatch);
+            }
+            catch { }
+        }
+
+        Socket socketSend;
+        /// <summary>
+        /// 等待客户端的连接，并且创建与之通信的Socket
+        /// </summary>
+        /// <param name="o"></param>
+        void Listen(object o)
+        {
+            Socket socketWatch = o as Socket;
+            //等待客户端的连接，并且创建一个负责通信的Socket
+            while (true)
+            {
+                try
+                {
+                    //负责跟客户端通信的Socket
+                    socketSend = socketWatch.Accept();
+                    //将远程连接的客户端的IP地址和Socket存入集合中
+                    dicSocket.Add(socketSend.RemoteEndPoint.ToString(), socketSend);
+                    //将远程连接的客户端的IP地址和端口号存储下拉框中
+                    cboUsers.Items.Add(socketSend.RemoteEndPoint.ToString());
+                    //192.168.0.100:连接成功
+                    Dispatcher.BeginInvoke(DispatcherPriority.Normal, new delegate1(ShowMsg), socketSend.RemoteEndPoint.ToString() + ":" + "连接成功");
+                    //开启一个新的线程，不停地接收客户端发送过来的消息
+                    Thread th = new Thread(Receive);
+                    th.IsBackground = true;
+                    th.Start(socketSend);
+                }
+                catch { }
+            }
+        }
+
+        //将远程连接的客户端的IP地址和Socket存入集合中
+        Dictionary<string, Socket> dicSocket = new Dictionary<string, Socket>();
+
+        /// <summary>
+        /// 服务端不停地接收客户端发送过来的消息
+        /// </summary>
+        /// <param name="o"></param>
+        void Receive(object o)
+        {
+            Socket socketSend = o as Socket;
+            while (true)
+            {
+                try
+                {
+                    //客户端连接成功后，服务器应该接收服务器应该接收客户端发来的消息
+                    byte[] buffer = new byte[1024 * 1024 * 2];
+                    //实际接收到的有效字节数
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    string str = Encoding.UTF8.GetString(buffer, 0, r);
+                    Dispatcher.BeginInvoke(DispatcherPriority.Normal, new delegate1(ShowMsg), socketSend.RemoteEndPoint.ToString() + ":" + str);
+                }
+                catch { }
+            }
+        }
+
+        private void ShowMsg(string str)
+        {
+            txtContent.AppendText(str + "\r\n");
+        }
+
+        /// <summary>
+        /// 服务器给客户端发送消息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSendMsg_Click(object sender, RoutedEventArgs e)
+        {
+            string str = txtSendMsg.Text.Trim();
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+            //socketSend.Send(buffer);
+            //获得用户在下拉框中选中的IP地址
+            string ip = cboUsers.SelectedItem.ToString();
+            dicSocket[ip].Send(buffer);
+        }
+    }
+}
